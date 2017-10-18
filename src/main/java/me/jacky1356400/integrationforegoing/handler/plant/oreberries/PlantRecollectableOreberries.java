3c060739e806ec5f87a6091a08abdbb1b1e3557a
Jacky1356400 <jacky1356400@hotmail.com>
2017-10-19 02:38:03 +0800

    1.3 Update
    - Updated stuff
    - Redone Plant Recollector integrations
    - Added Plant Recollector integration for Extra Utilities 2 ender lilly

---
+++ src/main/java/me/jacky1356400/integrationforegoing/handler/plant/oreberries/PlantRecollectableOreberries.java
@@ -0,0 +1,50 @@
+package me.jacky1356400.integrationforegoing.handler.plant.oreberries;
+
+import com.buuz135.industrial.api.plant.PlantRecollectable;
+import me.jacky1356400.integrationforegoing.util.ModUtils;
+import me.jacky1356400.integrationforegoing.util.Reference;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.registries.IForgeRegistry;
+
+import java.util.List;
+
+public class PlantRecollectableOreberries {
+
+    @SubscribeEvent
+    public void register(RegistryEvent.Register<PlantRecollectable> event) {
+        IForgeRegistry<PlantRecollectable> registry = event.getRegistry();
+        registry.register(new PlantRecollectable("oreberries") {
+            @Override
+            public boolean canBeHarvested(World world, BlockPos pos, IBlockState blockState) {
+                Block block = blockState.getBlock();
+                if(block.getMetaFromState(blockState) != 3) return false;
+                ResourceLocation registryName = block.getRegistryName();
+                return registryName.getResourceDomain().equals("oreberries")
+                        && registryName.getResourcePath().endsWith("_oreberry_bush");
+            }
+
+            @Override
+            public List<ItemStack> doHarvestOperation(World world, BlockPos pos, IBlockState blockState) {
+                NonNullList<ItemStack> stacks = NonNullList.create();
+                String name = blockState.getBlock().getRegistryName().getResourcePath();
+                stacks.add(0, ModUtils.getItemStackByObject("oreberries", name.substring(0, name.length() - 5), world.rand.nextInt(3) + 1, 0));
+                world.setBlockState(pos, blockState.withProperty(Reference.OREBERRIES_AGE, 2));
+                return stacks;
+            }
+
+            @Override
+            public boolean shouldCheckNextPlant(World world, BlockPos pos, IBlockState blockState) {
+                return true;
+            }
+        });
+    }
+
+}
--- src/main/java/me/jacky1356400/integrationforegoing/util/Reference.java
+++ src/main/java/me/jacky1356400/integrationforegoing/util/Reference.java
@@ -6,7 +6,7 @@ public class Reference {
 
     public static final String MODID = "integrationforegoing";
     public static final String MODNAME = "IntegrationForegoing";
-    public static final String VERSION = "1.12-1.2.1";
+    public static final String VERSION = "1.12-1.3";
     public static final String MCVERSION = "[1.12,)";
     public static final String DEPENDS
             = "required-after:industrialforegoing@[1.12-1.3-58,);"
@@ -25,5 +25,6 @@ public class Reference {
     public static final String PROXY = "me.jacky1356400.integrationforegoing.proxy";
 
     public static final PropertyInteger OREBERRIES_AGE = PropertyInteger.create("age", 0, 3);
+    public static final PropertyInteger ENDERLILLY_AGE = PropertyInteger.create("growth", 0, 7);
 
 }
