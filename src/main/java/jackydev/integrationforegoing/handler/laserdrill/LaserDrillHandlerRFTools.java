e44dc23d4b8510163050627b72faa78ae7d65a06
Joseph C. Sible <josephcsible@users.noreply.github.com>
2017-12-08 23:13:12 -0500

    Add the nether and end variants of dimensional shard ore (#14)

--- src/main/java/jackydev/integrationforegoing/handler/laserdrill/LaserDrillHandlerRFTools.java
+++ src/main/java/jackydev/integrationforegoing/handler/laserdrill/LaserDrillHandlerRFTools.java
@@ -5,7 +5,9 @@ import jackydev.integrationforegoing.util.ModUtils;
 public class LaserDrillHandlerRFTools {
 
     public static void init() {
-        ModUtils.addCustomLaserDrillEntryBlock(0, "rftools", "dimensional_shard_ore", 1, 0, 3);
+        ModUtils.addCustomLaserDrillEntryBlock(0, "rftools", "dimensional_shard_ore", 1, 0, 1);
+        ModUtils.addCustomLaserDrillEntryBlock(0, "rftools", "dimensional_shard_ore", 1, 1, 1);
+        ModUtils.addCustomLaserDrillEntryBlock(0, "rftools", "dimensional_shard_ore", 1, 2, 1);
     }
 
 }
