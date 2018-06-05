e113a9824006aabbddb139c124c97e1d556a9fb4
Jacky Scrubby <jacky1356400@gmail.com>
2018-06-06 03:34:09 +0800

    Updatesssssss

--- src/main/java/jackyy/integrationforegoing/util/Reference.java
+++ src/main/java/jackyy/integrationforegoing/util/Reference.java
@@ -6,7 +6,7 @@ import net.minecraft.block.properties.PropertyInteger;
 public final class Reference {
     public static final String MODID = "integrationforegoing";
     public static final String MODNAME = "Integration Foregoing";
-    public static final String VERSION = "1.12-1.6";
+    public static final String VERSION = "1.12-1.6.1";
     public static final String MCVERSION = "[1.12,1.13)";
     public static final String DEPENDS
             = "required-after:" + ModNames.IF + "@[1.12.2-1.6.5,);"
