96f2bba13e2de13d0134a5c6e044df50aa90ca4a
Jacky Scrubby <jacky1356400@gmail.com>
2018-08-29 20:57:53 +0800

    1.7.4 Update
    - Fixed mod crashing with version 1.10.6 (or newer) of Industrial Foregoing (again) by properly enforcing the minimum mod version, closes #31

--- src/main/java/jackyy/integrationforegoing/util/Reference.java
+++ src/main/java/jackyy/integrationforegoing/util/Reference.java
@@ -9,10 +9,10 @@ public final class Reference {
 
     public static final String MODID = "integrationforegoing";
     public static final String MODNAME = "Integration Foregoing";
-    public static final String VERSION = "1.12.2-1.7.3";
+    public static final String VERSION = "1.12.2-1.7.4";
     public static final String MCVERSION = "[1.12,1.13)";
     public static final String DEPENDS
-            = "required-after:" + IF + "@[1.12.2-1.10.6,);"
+            = "required-after:" + IF + "@[1.10.6,);"
             + "after:" + TF + ";"
             + "after:" + IE + ";"
             + "after:" + TCON + ";"
